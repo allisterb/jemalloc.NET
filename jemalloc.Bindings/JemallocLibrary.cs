18f8f69f3f40a1abb9e8b7a24bb83a500c161fd1
Allister Beharry <allister.beharry@gmail.com>
2017-11-11 07:48:15 -0400

    Use jemalloc-cs.h as input to CppSharp. CppSharp run completes successfully.

---
+++ jemalloc.Bindings/JemallocLibrary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Generators;
+
+namespace jemalloc.Bindings
+{
+    public class JemallocLibrary : ILibrary
+    {
+        /// Setup the driver options here.
+        public void Setup(Driver driver)
+        {
+            DriverOptions options = driver.Options;
+            options.GeneratorKind = GeneratorKind.CSharp;
+            Module module = options.AddModule("jemalloc");
+            module.Defines.AddRange(@"JEMALLOC_NO_PRIVATE_NAMESPACE;REENTRANT;WINDLL;DLLEXPORT;JEMALLOC_DEBUG;DEBUG".Split(';'));
+            module.IncludeDirs.Add(@"..\..\..\..\jemalloc\include\jemalloc");
+            module.IncludeDirs.Add(@"..\..\..\..\jemalloc\include\jemalloc\internal");
+            module.IncludeDirs.Add(@"..\..\..\..\jemalloc\include\msvc_compat");
+            module.IncludeDirs.Add(@".\");
+            module.Headers.Add("jemalloc-cs.h");
+            
+            module.LibraryDirs.Add(@"..\..\..\..\x64\Debug\");
+            //module.Libraries.Add("jemallocd.lib");
+        }
+
+        /// Setup your passes here.
+        public void SetupPasses(Driver driver)
+        {
+           
+        }
+
+        /// Do transformations that should happen before passes are processed.
+        public void Preprocess(Driver driver, ASTContext ctx)
+        {
+
+        }
+
+        /// Do transformations that should happen after passes are processed.
+        public void Postprocess(Driver driver, ASTContext ctx)
+        {
+
+        }
+    }
+}
--- jemalloc.Bindings/Program.cs
+++ jemalloc.Bindings/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CppSharp;
+
 namespace jemalloc.Bindings
 {
     class Program
     {
         static void Main(string[] args)
         {
+            ConsoleDriver.Run(new JemallocLibrary());
         }
     }
 }
